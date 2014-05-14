488d3f4e8bc6c04f3c935eefbadbc8e580564769
Rabih <somcom3@gmail.com>
2014-05-14 07:27:36 -0400

    Fix CarbonApp RomList
    Remove Settings Override
    Change-Id: Ifba29510e02ba68fb3590c8cf25b5e7985db4c22

--- carbon.mk
+++ carbon.mk
@@ -3,7 +3,7 @@ $(call inherit-product, device/samsung/skyrocket/full_skyrocket.mk)
 # Inherit some common Carbon stuff.
 $(call inherit-product, vendor/carbon/config/common_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGH-I727 TARGET_DEVICE=SGH-I727 BUILD_FINGERPRINT="samsung/SGH-I727/SGH-I727:4.1.2/JZO54K/I727UCMC1:user/release-keys" PRIVATE_BUILD_DESC="SGH-I727-user 4.1.2 JZO54K I727UCMC1 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGH-I727 BUILD_FINGERPRINT="samsung/SGH-I727/SGH-I727:4.1.2/JZO54K/I727UCMC1:user/release-keys" PRIVATE_BUILD_DESC="SGH-I727-user 4.1.2 JZO54K I727UCMC1 release-keys"
 
 PRODUCT_NAME := carbon_skyrocket
 PRODUCT_DEVICE := skyrocket
